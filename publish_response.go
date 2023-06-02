25695f909d5e8076e34e1d66c09e2d29a169987e
anthony <aahewins@protonmail.com>
2023-06-01 20:57:02 -0400

    int unix timestamp

--- publish_response.go
+++ publish_response.go
@@ -1,12 +1,10 @@
 package gotfy
 
-import "time"
-
 type PublishResp struct {
-	ID      string    `json:"id"`      // :"bUhbhgmmbeW0"
-	Time    time.Time `json:"time"`    // :1685150791
-	Expires time.Time `json:"expires"` // :1685193991
-	Event   string    `json:"event"`   // :"message"
-	Topic   string    `json:"topic"`   // :"Server"
-	Message string    `json:"message"` // :"triggered"
+	ID      string `json:"id"`      // :"bUhbhgmmbeW0"
+	Time    int    `json:"time"`    // :1685150791
+	Expires int    `json:"expires"` // :1685193991
+	Event   string `json:"event"`   // :"message"
+	Topic   string `json:"topic"`   // :"Server"
+	Message string `json:"message"` // :"triggered"
 }
